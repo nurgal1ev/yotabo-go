c0434d02880b3b3b4121bd4ae24d471bf36ad610
nurgal1ev <nurgaliev0644@gmail.com>
2026-02-19 10:55:08 +0300

    fix StartServer()

--- internal/transport/httpv1/server.go
+++ internal/transport/httpv1/server.go
@@ -2,6 +2,7 @@ package httpv1
 
 import (
 	"github.com/danielgtaylor/huma/v2"
+	"github.com/danielgtaylor/huma/v2/adapters/humachi"
 	"github.com/go-chi/chi/v5"
 	"github.com/nurgal1ev/yotabo-go/internal/transport/httpv1/user"
 	"net/http"
@@ -10,13 +11,12 @@ import (
 func StartServer() {
 	r := chi.NewMux()
 
-	api := huma.NewAPI(huma.DefaultConfig("Yotabo API", "1.0.0"), r)
+	api := humachi.New(r, huma.DefaultConfig("Yotabo API", "1.0.0"))
 
 	huma.Post(api, "/api/v1/auth/register", user.RegisterHandler)
 	huma.Post(api, "/api/v1/auth/login", user.LoginHandler)
 
-	err := http.ListenAndServe(":8080", r)
-	if err != nil {
+	if err := http.ListenAndServe(":8080", r); err != nil {
 		panic(err)
 	}
 }
